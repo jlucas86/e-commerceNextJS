c13202d6be701d297c0a8c191cec2a85cb5f04fc
jlucas86 <joshua.lucas014@gmail.com>
2024-02-24 23:57:29 -0500

    added login page

---
+++ e-commerce-nextjs/src/app/login/page.tsx
@@ -0,0 +1,14 @@
+export default function login() {
+    return(
+        <div>
+            <h1>Log In</h1>
+            <form action="" method="post">
+                <input type="text" placeholder="Username" />
+                <br />
+                <input type="password" placeholder="Password"/>
+                <br />
+                <button>Login</button>
+            </form>
+        </div>
+    );
+}
