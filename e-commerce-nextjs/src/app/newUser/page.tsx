6604ca2c464cb4e4cf84605fb96109876e533d67
jlucas86 <joshua.lucas014@gmail.com>
2024-02-24 23:51:37 -0500

    new user creation page added

---
+++ e-commerce-nextjs/src/app/newUser/page.tsx
@@ -0,0 +1,18 @@
+export default function newUser() {
+    return (
+        <div className=" w-screen h-screen flex flex-col items-center justify-center border-2 border-white radious ">
+            <h1>New User</h1>
+            <form action="">
+                <input type="text" placeholder="Username" />
+                <br />
+                <input type="text" placeholder="Email" />
+                <br />
+                <input type="password" placeholder="Password" />
+                <br />
+                <input type="password" placeholder="confirm Password" />
+                <br />
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+}
