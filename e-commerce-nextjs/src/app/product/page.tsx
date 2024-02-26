f5d818d9c18908960e3078b05d5ba3bb81e787b5
jlucas86 <joshua.lucas014@gmail.com>
2024-02-25 23:56:59 -0500

    added image changer to product page

--- e-commerce-nextjs/src/app/product/page.tsx
+++ e-commerce-nextjs/src/app/product/page.tsx
@@ -1,9 +1,34 @@
+'use client'
+
+import { useState } from "react";
 
 export default function product() {
 
+    let imgs : string[] = ["https://picsum.photos/200/300", "https://picsum.photos/200/200","https://picsum.photos/300/300","https://picsum.photos/400/300"]    
+
+    const [mainImg, setMainImg] = useState<string>("https://picsum.photos/200/300")
+
+    const changeMain = (i:string) =>{
+        setMainImg(i);
+    }
+    
     return(
         <div className= " w-screen h-screen flex">
-            <div> image wraper</div>
+            <div>
+                <div>
+                    <img src={mainImg} alt="" />
+                </div>
+                <div className=" flex">
+                    {imgs.map((i)=>{
+                        return (
+                            <div onClick={()=>changeMain(i)} >
+                                <img src={i} alt="none" />
+                            </div>
+                        );
+                    })}
+                </div>
+
+            </div>
             <div className=" w-full h-screen border-2 border-white rounded bg-slate-900 shadow-lg">
                 <h1>product name</h1>
                 <h3>price</h3>
