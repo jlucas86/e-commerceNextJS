bc5e2e18fe374bc3047cb5d673b6c58762c2dcf8
jlucas86 <joshua.lucas014@gmail.com>
2024-02-25 20:12:22 -0500

    updated user page with basics

---
+++ e-commerce-nextjs/src/app/user/page.tsx
@@ -0,0 +1,15 @@
+export default function user() {
+    return(
+        <div>
+            user page
+            <div className=" flex flex-wrap">
+                <div className=" w-screen lg:w-1/2 border border-2 border-white rounded">orders</div>
+                <div className=" w-screen lg:w-1/2 border border-2 border-white rounded">payment methods</div>
+                <div className=" w-screen lg:w-1/2 border border-2 border-white rounded">stores</div>
+                <div className=" w-screen lg:w-1/2 border border-2 border-white rounded">user info</div>
+            </div>
+            
+
+        </div>
+    );
+}
