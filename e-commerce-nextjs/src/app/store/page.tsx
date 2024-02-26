214679551b26e034680cd5f3cc67febc3a1094d6
jlucas86 <joshua.lucas014@gmail.com>
2024-02-25 23:07:12 -0500

    added store page with basic design

---
+++ e-commerce-nextjs/src/app/store/page.tsx
@@ -0,0 +1,20 @@
+export default function store() {
+    return (
+        <div className=" w-screen h-screen">
+            <h1>store name</h1>
+            <div className="  flex border border-2 border-white rounded">
+                <div className=" border border-2 border-white rounded">
+                    filters
+                    <div>
+                        <h3>Type</h3>
+                    </div>
+                </div>
+                <div className=" w-full flex flex-wrap border border-2 border-white rounded">
+
+                </div>
+
+                
+            </div>
+        </div>
+    );
+}
