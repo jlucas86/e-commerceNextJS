b16ebcd244702ce8285708fc582941ba881b52d0
jlucas86 <joshua.lucas014@gmail.com>
2024-02-28 23:48:21 -0500

    changed header style

--- e-commerce-nextjs/src/app/componets/header/header.tsx
+++ e-commerce-nextjs/src/app/componets/header/header.tsx
@@ -1,7 +1,7 @@
 export default function Header(prosp:{}) {
     
     return (
-        <div className="flex flex-col justify-between lg:flex-row w-screen border-black border-solid border-2 rounded">
+        <div className="flex flex-col justify-between lg:flex-row w-screen  rounded">
             <button>menu</button>
             <h1>buy lots</h1>
             <div>
