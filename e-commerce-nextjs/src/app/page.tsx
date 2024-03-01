8a2e8ee17f6691ef2ccee6da394e78f14c0e455f
jlucas86 <joshua.lucas014@gmail.com>
2024-02-29 23:37:37 -0500

    added serch page

--- e-commerce-nextjs/src/app/page.tsx
+++ e-commerce-nextjs/src/app/page.tsx
@@ -8,7 +8,7 @@ export default function home() {
       <Header></Header>
       <div className=" flex">
         <FilterMenu></FilterMenu>
-        <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5  gap-1 w-full">
+        <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6  gap-1 w-full">
                     <Product img={"https://picsum.photos/200/300"} imgAlt="randome photo" title={"title"} price={200}></Product>
                     <Product img={"https://picsum.photos/200/300"} imgAlt="randome photo" title={"title"} price={200}></Product>
                     <Product img={"https://picsum.photos/200/300"} imgAlt="randome photo" title={"title"} price={200}></Product>
