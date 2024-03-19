fcc01456d9a0b2f7b558d8a3f9e5d58701fa09b0
jlucas86 <joshua.lucas014@gmail.com>
2024-03-19 05:30:26 -0400

    begain to implement adding a store to the data base

--- e-commerce-nextjs/src/app/user/componets/store.tsx
+++ e-commerce-nextjs/src/app/user/componets/store.tsx
@@ -1,3 +1,36 @@
+'use client'
+
+import { useState } from "react";
 export default function store(props:{}) {
+
+
+    const [adding, setAdding] = useState<boolean>(false)
+    const [name, setName] = useState<string>("");
+    const [description, setDescription] = useState<string>("");
+
+    const addStore = async () => {
+        
+        setAdding(false)
+    }
     
+    return(
+        <div className=" w-screen lg:w-1/2 border-2 border-white rounded" >
+            <div>
+                list of stores this will be done by mapping a list got from the api
+            </div>
+            <button onClick={() => setAdding(!adding)}>
+                + add store
+            </button>
+            {adding == true &&
+                <div className=" ml-4">
+                    <input type="text" placeholder="Name" onChange={e => setName(e.target.value)} />
+                    <br />
+                    <input type="text" placeholder="Description" onChange={e => setDescription(e.target.value)} />
+                    <br />
+                    <button onClick={addStore}>Add Store</button>
+                </div>
+            }
+            
+        </div>
+    );
 }
--- e-commerce-nextjs/src/app/user/page.tsx
+++ e-commerce-nextjs/src/app/user/page.tsx
@@ -1,3 +1,5 @@
+import Store from "./componets/store";
+
 export default function user() {
     return(
         <div>
@@ -5,6 +7,7 @@ export default function user() {
             <div className=" flex flex-wrap">
                 <div className=" w-screen lg:w-1/2 border border-2 border-white rounded">orders</div>
                 <div className=" w-screen lg:w-1/2 border border-2 border-white rounded">payment methods</div>
+                <Store></Store>
                 <div className=" w-screen lg:w-1/2 border border-2 border-white rounded">stores</div>
                 <div className=" w-screen lg:w-1/2 border border-2 border-white rounded">user info</div>
             </div>
