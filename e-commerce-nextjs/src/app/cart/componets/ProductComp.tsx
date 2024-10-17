a5b8805f73e5368b452a67e8fe4f18c05b8f37a8
jlucas86 <joshua.lucas014@gmail.com>
2024-10-17 01:00:02 -0400

    added quantiy text box to productcomp.tsx

--- e-commerce-nextjs/src/app/cart/componets/ProductComp.tsx
+++ e-commerce-nextjs/src/app/cart/componets/ProductComp.tsx
@@ -1,17 +1,41 @@
+'use client'
+
+import { ChangeEventHandler, useState } from "react";
 import { product } from "../interfaces/interfaces";
 
 
 
 export default function ProductComp(props:{p:product}) {
 
+    const [q, setQ] = useState<number>(0)
+
+    const changeQ = (e: ChangeEventHandler<HTMLInputElement>) =>{
+
+        
+        if (e.target.value !== ''){
+            setQ(+e.target.value)
+            console.log("herre", +e.target.value)
+        }
+            
+    }
+
     return(
         <div className="flex justify-between">
-                                <div className="flex">
+                                <div className="flex w-full bg-orange-600">
                                     <img src={props.p.image} alt="" />
-                                    <div className=" flex-col">
-                                        <div>{props.p.name}</div>
-                                        <div>{props.p.description}</div>
+                                    <div className="flex justify-between w-full">
+                                        <div className=" flex-col">
+                                            <div>{props.p.name}</div>
+                                            <div>{props.p.description}</div>
+                                        </div>
+                                        <div className="text-black">
+                                            <form action="">
+                                                <input type="number" onChange={changeQ} defaultValue={q} />
+                                            </form>
+                                            
+                                        </div>
                                     </div>
+                                    
                                 </div>
                                 
                                 <div>
