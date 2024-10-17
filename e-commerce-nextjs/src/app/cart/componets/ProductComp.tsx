97b364da55920d3e9e1c7daaefe2224186ea4fd0
jlucas86 <joshua.lucas014@gmail.com>
2024-10-17 01:01:34 -0400

    fixed formating of ProductComp.tsx

--- e-commerce-nextjs/src/app/cart/componets/ProductComp.tsx
+++ e-commerce-nextjs/src/app/cart/componets/ProductComp.tsx
@@ -9,39 +9,37 @@ export default function ProductComp(props:{p:product}) {
 
     const [q, setQ] = useState<number>(0)
 
-    const changeQ = (e: ChangeEventHandler<HTMLInputElement>) =>{
+    const changeQ = (e: ChangeEventHandler<HTMLInputElement> ) =>{
 
-        
         if (e.target.value !== ''){
             setQ(+e.target.value)
             console.log("herre", +e.target.value)
-        }
-            
+        }     
     }
 
     return(
         <div className="flex justify-between">
-                                <div className="flex w-full bg-orange-600">
-                                    <img src={props.p.image} alt="" />
-                                    <div className="flex justify-between w-full">
-                                        <div className=" flex-col">
-                                            <div>{props.p.name}</div>
-                                            <div>{props.p.description}</div>
-                                        </div>
-                                        <div className="text-black">
-                                            <form action="">
-                                                <input type="number" onChange={changeQ} defaultValue={q} />
-                                            </form>
-                                            
-                                        </div>
-                                    </div>
-                                    
-                                </div>
-                                
-                                <div>
-                                    {props.p.price.toString()}
-                                </div>
-                            </div>
+            <div className="flex w-full bg-orange-600">
+                <img src={props.p.image} alt="" />
+                <div className="flex justify-between w-full">
+                    <div className=" flex-col">
+                        <div>{props.p.name}</div>
+                        <div>{props.p.description}</div>
+                    </div>
+                    <div className="text-black">
+                        <form action="">
+                            <input type="number" onChange={changeQ} defaultValue={q} />
+                        </form>
+                        
+                    </div>
+                </div>
+                
+            </div>
+            
+            <div>
+                {props.p.price.toString()}
+            </div>
+        </div>
     )
     
 }
