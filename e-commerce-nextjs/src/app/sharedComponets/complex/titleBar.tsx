fc48a8a8c03b9e169952afd9847b691041b4e6c7
jlucas86 <joshua.lucas014@gmail.com>
2024-11-11 02:47:23 -0500

    updated css for input globaly also upded css for title bar and added buttons

--- e-commerce-nextjs/src/app/sharedComponets/complex/titleBar.tsx
+++ e-commerce-nextjs/src/app/sharedComponets/complex/titleBar.tsx
@@ -1,16 +1,42 @@
+'use client'
+
+
 export default function TitleBar() {
+
+    const menuButtonClick = () =>{
+        console.log("menu click")
+    }
+
+    const searchButtonClick = () =>{
+        console.log("search click")
+    }
+
+    const accountButtonClick = () =>{
+        console.log("account click")
+    }
+
+    const  homeClick = () =>{
+        console.log("home click")
+        
+    }
+
     return (
         <div className=" h-14 w-screen px-7 flex justify-between items-center bg-white shadow" >
             <div className=" flex justify-center items-center">
-                <div>menu</div>
-                <div className=" flex  border rounded-3xl  my-2">
-                    <input type="text" name="searchBar" id="searchBar"  />
-                    <button className=" rounded-3xl bg-purple-600"> search</button>
+                <div>
+                    <button onClick={menuButtonClick}>menu</button>
+                </div>
+                <div className=" flex  border-purple-300 border-2 rounded-3xl  my-2 bg-purple-300 ">
+                    <input type="text" name="searchBar" id="searchBar" className=" rounded-3xl bg-purple-300 border-none px-2" />
+                    <button className=" px-2 rounded-3xl bg-purple-600" onClick={searchButtonClick}> search</button>
                 </div>
                 
 
             </div>
-            <div>websit name</div>
+            <a href="/">
+                <button >websit name</button> 
+            </a>
+            <div onClick={homeClick}></div>
             <div>account stuff</div>
         </div>)
 }
