7726806bc8ac4ea1af85bb9c4a128986889e2ff3
jlucas86 <joshua.lucas014@gmail.com>
2024-11-10 23:00:13 -0500

    added menu, acount stuff, and an unfucntional search bar to title bar

--- e-commerce-nextjs/src/app/sharedComponets/complex/titleBar.tsx
+++ e-commerce-nextjs/src/app/sharedComponets/complex/titleBar.tsx
@@ -1,6 +1,16 @@
 export default function TitleBar() {
     return (
-        <div className=" h-10 w-screen text-center bg-white shadow" >
-            title bar
+        <div className=" h-14 w-screen px-7 flex justify-between items-center bg-white shadow" >
+            <div className=" flex justify-center items-center">
+                <div>menu</div>
+                <div className=" flex  border rounded-3xl  my-2">
+                    <input type="text" name="searchBar" id="searchBar"  />
+                    <button className=" rounded-3xl bg-purple-600"> search</button>
+                </div>
+                
+
+            </div>
+            <div>websit name</div>
+            <div>account stuff</div>
         </div>)
 }
