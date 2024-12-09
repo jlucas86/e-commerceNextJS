5326b024bb64d791814ed23cd73e864b6a308658
jlucas86 <joshua.lucas014@gmail.com>
2024-12-08 23:46:56 -0500

    began re working user page to be more intuitive

--- e-commerce-nextjs/src/app/user/page.tsx
+++ e-commerce-nextjs/src/app/user/page.tsx
@@ -1,9 +1,34 @@
+'use client'
+
+import TitleBar from "../sharedComponets/complex/titleBar";
 import Store from "./componets/store";
 
 export default function user() {
     return(
         <div>
-            user page
+            
+            {/* <TitleBar></TitleBar> */}
+
+            <div className=" flex justify-items-end">
+                <div className=" bg-gray-200 w-1/2 border border-gray-400 rounded ">
+                    <div>
+                        orders
+                    </div>
+                    <div>
+                        stores
+                    </div>
+                    <div>
+                        account info
+                    </div>
+                    <div>
+                        security
+                    </div>
+                </div>
+                <div className=" bg-white w-1/2  border border-gray-400 rounded ">
+                    menu information
+                </div>
+            </div>
+
             <div className=" flex flex-wrap">
                 <div className=" w-screen lg:w-1/2 border border-2 border-white rounded">orders</div>
                 <div className=" w-screen lg:w-1/2 border border-2 border-white rounded">payment methods</div>
