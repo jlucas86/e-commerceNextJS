89cd935a5b265f3da3a274b828c299dc1b1dc19d
jlucas86 <joshua.lucas014@gmail.com>
2024-12-08 22:48:26 -0500

    added basic structure for accountMenu

--- e-commerce-nextjs/src/app/sharedComponets/complex/accountMenu.tsx
+++ e-commerce-nextjs/src/app/sharedComponets/complex/accountMenu.tsx
@@ -1,7 +1,8 @@
 export default function AccountMenu() {
     return(
         <div className=" absolute top-14 right-0 border border-gray-400 rounded bg-white p-3 opacity-100">
-            account menu
+            <div> order history</div>
+            <div> account details</div>
         </div>
     );
 }
