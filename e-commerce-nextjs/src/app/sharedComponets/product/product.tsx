6dd56cfe4fced5742b22d9132457012617825abe
jlucas86 <joshua.lucas014@gmail.com>
2025-01-03 21:47:38 -0500

    updated css for button in product preview

--- e-commerce-nextjs/src/app/sharedComponets/product/product.tsx
+++ e-commerce-nextjs/src/app/sharedComponets/product/product.tsx
@@ -2,13 +2,13 @@ export default function Product(props:{img:string, imgAlt:string, title:string, 
     return (
         <div className="justify-items-start w-full p-2 mt-2 bg-white border-white shadow border rounded">
             <img className="w-full rounded" src={props.img} alt={props.imgAlt} />
-            <div className=" w-full bg-orange-500 w-">
+            <div className=" w-full ">
                 <h1>price</h1>
                 <h2>title</h2>
                 <h4>store/seller</h4>
                 <h4>review</h4>
-                <h3>description</h3>
-                <button>add to cart</button>
+                <p>description</p>
+                <button className=" bg-green-500 rounded px-2 py-1" >add to cart</button>
                 
                 
             </div>
