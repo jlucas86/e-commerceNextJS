36e92926591ae75f2983b1d30754483f8cd7ab83
jlucas86 <joshua.lucas014@gmail.com>
2025-03-06 02:21:14 -0500

    made store sub-menu in user collapsible

--- e-commerce-nextjs/src/app/user/componets/store_componets/storeEntity.tsx
+++ e-commerce-nextjs/src/app/user/componets/store_componets/storeEntity.tsx
@@ -1,22 +1,51 @@
 import { useEffect, useState } from "react";
 import AddProductMenu from "./addProductMenu";
+import Product from "./product";
 
 export default function StoreEntity(props:{storeId:Number}) {
+
+    // style for the main div that is responsable for showing the sotre or now
+    const [showMenu, setShowMenu] = useState<Boolean>(false)
 
     // state var used to keep track of when any product is changed and this 
     // componet needs rerendered
     const [productChange, setProductChange] = useState<Boolean>(false)
 
+
+
     useEffect(()=>{
         // get a new list of products form the data base because a new one was added
     }, [productChange])
+
+    // menu 
+    const StoreMenu = () =>{
+        return(
+            <div>
+                <div className=" border-2 rounded">
+                    <h3> store name</h3>
+                    <p>desciption</p>
+                    list of products
+                    <Product productId={1}/>
+                </div>
+                <AddProductMenu storeId={props.storeId} />
+                <button onClick={()=>{setShowMenu(false)}}> hide menu</button>
+            </div>
+        )
+    }
     
     return(
         <div>
-            <div className=" border-2 rounded">
-                list of products
-            </div>
-            <AddProductMenu storeId={props.storeId} />
+            {showMenu? <div>
+                    <div>
+                        <StoreMenu />
+                    </div>
+                </div>:<div>
+                    <div onClick={()=>{setShowMenu(true)}}>
+                        store name
+                    </div>     
+                </div>}
+            
+            
         </div>
     );
 }
